b65eb1c8063ebe193d1cd832c6e53975ed8234b0
David Jurelius <davan839@gmail.com>
2020-11-26 10:50:35 +0100

    tweaks and memory change

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,22 @@ const html_table_to_json = require('html-table-to-json');
 const log = functions.logger.info;
 
 const runtimeOpts = {
-  timeoutSeconds: 300,
-  memory: '1GB'
+  timeoutSeconds: 20,
+  memory: '2GB'
 }
+const region = 'europe-west1'
 
 exports.instrument_table = functions
   .runWith(runtimeOpts)
-  .region('europe-west1')
+  .region(region)
   .https.onRequest(async (req, res) => {
 
   let { url, selector = '', validation_selector = '' } = req.query
+
+  if (!url) {
+    return res.send(`Invalid url: ${url}`);
+  }
+
   url = decodeURIComponent(url);
   selector = decodeURIComponent(selector);
   validation_selector = decodeURIComponent(validation_selector);
@@ -23,14 +29,17 @@ exports.instrument_table = functions
     // [START puppeteer-block]
     // launch Puppeteer and start a Chrome DevTools Protocol (CDP) session
     // with performance tracking enabled.
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox"]
+    });
     const page = await browser.newPage();
     // const client = await page.target().createCDPSession();
     // await client.send('Performance.enable');
 
     // browse to the page, capture and write the performance metrics
-    log(`Fetching url: ${url} with selector: ${selector}`);
-    await page.goto(url || "https://handelsbanken-marknadsinformation.se/app/instrument/SHBSABTIi/2365/Historik");
+    log(`Fetching url: ${url} with selector: ${selector} and validation_selector: ${validation_selector}`);
+    await page.setViewport({ width: 500, height: 1000 })
+    await page.goto(url, { waitUntil: "networkidle2" });
 
     await page.waitForSelector(validation_selector || "instrumenthistory history table tbody > tr:nth-child(1) > td.cell-table__num");
     const html_table = await page.$eval(selector, e => e.outerHTML);
