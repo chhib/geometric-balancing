9d1e88740e110f875c46b4805d787bc791d47f20
David Jurelius <davan839@gmail.com>
2020-11-26 11:10:02 +0100

    cleaning up

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const puppeteer = require('puppeteer');
 const html_table_to_json = require('html-table-to-json');
 const log = functions.logger.info;
-
 const runtimeOpts = {
   timeoutSeconds: 20,
   memory: '2GB'
@@ -28,43 +27,32 @@ exports.instrument_table = functions
   selector = decodeURIComponent(selector);
   validation_selector = decodeURIComponent(validation_selector);
 
-  log(cache);
+  // Serve from cache if less than 1 hour
   if (cache.json_table.length > 0 && cache.ttl > new Date()) {
     return res.json(cache.json_table);
   }
 
-
   let browser;
   try {
-    // [START puppeteer-block]
-    // launch Puppeteer and start a Chrome DevTools Protocol (CDP) session
-    // with performance tracking enabled.
     browser = await puppeteer.launch({
       args: ["--no-sandbox"]
     });
     const page = await browser.newPage();
-    // const client = await page.target().createCDPSession();
-    // await client.send('Performance.enable');
 
-    // browse to the page, capture and write the performance metrics
     log(`Fetching url: ${url} with selector: ${selector} and validation_selector: ${validation_selector}`);
     await page.setViewport({ width: 500, height: 1000 })
     await page.goto(url, { waitUntil: "networkidle2" });
-
-    await page.waitForSelector(validation_selector || "instrumenthistory history table tbody > tr:nth-child(1) > td.cell-table__num");
+    await page.waitForSelector(validation_selector);
     const html_table = await page.$eval(selector, e => e.outerHTML);
     const json_table = html_table_to_json.parse(html_table).results;
-      // Store fresh data in cache
+    
+    // Save to cache
     cache.json_table = json_table
-    // Store a TTL for the data
     const dateInOneHour = new Date()
     dateInOneHour.setHours(dateInOneHour.getHours() + 1);
     cache.ttl = dateInOneHour 
 
-    
-    //res.send(html_table)
     res.json(json_table);
-
   } catch (e) {
     console.error('Caught Error: '+e);
     res.status(500).send(e);
